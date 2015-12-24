73889768d70323cc02a09a12a161826b7a1e3665
Olexander Yermakov <mannavard1611@gmail.com>
2015-12-24 20:58:39 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ndbvalid',
-    version='1.0.0',
+    version='1.1.0',
     author='Olexander Yermakov',
     author_email='olexander.yermakov@gmail.com',
     url='https://github.com/alexyer/ndbvalid',
