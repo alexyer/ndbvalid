70ef2dad8536c60f06b32dd3e383bf96969365b0
Olexander Yermakov <mannavard1611@gmail.com>
2015-12-24 18:28:06 +0200

    Add sphinx, coverage and travis

---
+++ doc/index.rst
@@ -0,0 +1,9 @@
+.. ndbvalid documentation master file, created by
+   sphinx-quickstart on Thu Dec 24 18:23:59 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ndbvalid's documentation!
+====================================
+
+.. autofunction:: ndbvalid.length
--- ndbvalid.py
+++ ndbvalid.py
@@ -6,6 +6,17 @@ class NdbValidationError(Exception):
 
 
 def length(min=None, max=None):
+    """
+    Length validator.
+    The string must have a minimal length of min and maximal length of max characters.
+    
+    :param min: Minimum length
+    :param max: Maximal length
+    :type min: int
+    :type max: int
+    :rtype: None
+    :raises: NdbValidationError
+    """
     def length_validator(prop, value):
         if min and len(value) < min:
             raise NdbValidationError('{} must be less than {} characters'.format(prop, min))
